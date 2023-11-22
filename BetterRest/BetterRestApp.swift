f7be9be4ca092df29fc9f5247b6f097729467c10
Dannyung50 <olaleyedaniel16@gmail.com>
2023-11-22 15:48:38 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Olaleye Daniel Ifeoluwa on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
